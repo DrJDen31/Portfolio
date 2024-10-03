117b11e4e532405612ea0f46d3a18d3351d3bc3b
DrJDen31 <133166064+DrJDen31@users.noreply.github.com>
2024-10-02 23:37:49 -0400

    Update WIP.tsx
    Return button

--- src/components/WIP.tsx
+++ src/components/WIP.tsx
@@ -1,6 +1,8 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function WIP() {
+  const navigate = useNavigate();
   return (
     <Container className="p-4">
       <Row className="pt-4">
@@ -16,6 +18,17 @@ function WIP() {
           <p>
             This page is currently under construction. Please check again later!
           </p>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col md="auto" className="pt-2">
+          <Button 
+            variant="primary"
+            onClick={() => navigate("/")>
+            Return Home
+          </Button>
         </Col>
         <Col></Col>
       </Row>
